127b28ec58361afa843c30be415809ec60fe8648
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-04 01:19:15 +0530

    [aditya]: Flip Coint Head or Tails

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Basic_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+           int HeadOrTails=random.Next(0, 2);
+            
+            if (HeadOrTails < 0.5)
+            {
+                Console.WriteLine("Tails");
+            }
+            else
+            {
+                Console.WriteLine("Head");
+
+            }
+        }
+    }
+}
